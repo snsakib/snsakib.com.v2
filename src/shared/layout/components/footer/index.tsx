452a56b174f042883a31486e5c493481c3fc15b7
snsakib <hello@snsakib.com>
2022-03-07 10:26:49 +0600

    create footer component

---
+++ src/shared/layout/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+let Footer: FC = () => {
+  return(
+    <footer>
+      Footer
+    </footer>
+  )
+}
+
+export default Footer;
--- src/shared/layout/index.tsx
+++ src/shared/layout/index.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import './layout.scss';
 import Header from './components/header';
+import Footer from './components/footer';
 
 let Layout: FC = ({children}) => {
   return (
     <>
       <Header/>
       {children}
-      <div>Footer</div>
+      <Footer/>
     </>
   );
 };
