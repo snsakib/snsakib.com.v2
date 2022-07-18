559f3d377388c43a5fee156bd2395b9f179e55fa
snsakib <hello@snsakib.com>
2022-07-18 10:02:59 +0600

    add stylings in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import image from './assets/img/profile_picture.jpg';
+import classes from './home.module.scss';
 
 const Home: FC = () => {
   return (
     <>
-      <img src={image} alt="Syed Nazmus Sakib" />
+      <img className={classes.profile_picture} src={image} alt="Syed Nazmus Sakib" width={300} height={300}/>
       <p>
         Description
       </p>
