2ee0839a0f7f517b024168174014534c46d511f1
snsakib <hello@snsakib.com>
2022-07-20 09:51:45 +0600

    fix home page layour

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,25 @@
 import { FC } from "react";
-import image from './assets/img/profile_picture.jpg';
-import classes from './home.module.scss';
+import image from "./assets/img/profile_picture.jpg";
+import classes from "./home.module.scss";
 
 const Home: FC = () => {
   return (
-    <>
-      <img className={classes.profile_picture} src={image} alt="Syed Nazmus Sakib" width={300} height={300}/>
-      <p>
-        Description
-      </p>
-    </>
+    <div className={classes.container}>
+      {/* TODO: Implement srcset for responsive image */}
+      <img
+        className={classes.profile_picture}
+        src={image}
+        alt="Syed Nazmus Sakib"
+        width={200}
+        height={200}
+      />
+      <div>
+        <p>Hi, I'm S. N. Sakib</p>
+        <p>Description</p>
+        <p>Description</p>
+        <p>Description</p>
+      </div>
+    </div>
   );
 };
 
